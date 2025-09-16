dc4eb9f7ae6b16d2bcfc57e21ef9145c2b6c0193
gio <gio@GIOPC>
2025-09-16 15:51:40 +0400

    add button component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,9 +8,14 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-  constructor(library: FaIconLibrary) {
-    library.addIconPacks(fas);
-  }
+    @Input() buttonText: string = 'Button';
+    @Input() iconName: string = '';
 
-  // Use the names for the inputs `buttonText` and `iconName`.
+    @Output() buttonClick = new EventEmitter<void>();
+
+    onClick(): void {
+        this.buttonClick.emit();
+    }
+
+    // Use the names for the inputs `buttonText` and `iconName`.
 }
